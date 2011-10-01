45da7d076e46dae4ddb5d6f3df85fdb6aca08bd3
Tihomir Trifonov <t.trifonov@gmail.com>
2011-10-01 18:19:59 +0300

    commit

--- app1/settings.py
+++ app1/settings.py
@@ -12,7 +12,6 @@ MANAGERS = ADMINS
 import json
 with open('/home/dotcloud/environment.json') as f:
     env = json.load(f)
-print env
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
