dae5879922de190ff66dbe628411110dc9d112d9
Tihomir Trifonov <t.trifonov@gmail.com>
2011-10-01 18:35:33 +0300

    commit

--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
- 
+
+from django.contrib.auth.models import User
+
 def home(request):
+    us = User.objects.all()
     return HttpResponse("App 1<a href='/secure/'>Secure</a>")
 
 def secure(request):
