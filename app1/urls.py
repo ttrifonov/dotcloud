18faca601fc084cf5d186cf8c3b672b8b113073c
Tihomir Trifonov <t.trifonov@gmail.com>
2011-10-01 14:26:06 +0300

    commit

--- app1/urls.py
+++ app1/urls.py
@@ -5,11 +5,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'app1.views.home', name='home'),
-
     url(r'^secure/', 'app1.views.secure', name='secure'),
 
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', 'app1.views.home', name='home'),
 )
